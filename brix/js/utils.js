b35244fef272976d2c21a1230e34f80cbc3a7cf8
itsjoshhzxc <153428160+itsjoshhzxc@users.noreply.github.com>
2025-12-06 21:30:58 +0800

    ako si brix

---
+++ brix/js/utils.js
@@ -0,0 +1,7 @@
+// Logout function - basta tanan na dashboard
+function PagesLogout() {
+    localStorage.removeItem('userSession');
+    sessionStorage.clear();
+    
+    window.location.href = '../frontpage.html';
+}
