e4aa1da28d9aa48ee8d86ad5b75cc1173f384579
kenttolipas24 <kenttolipas24@gmail.com>
2025-12-13 12:57:05 +0800

    bomel dako putoy

--- kent/js/components-js/dean/deanNav.js
+++ kent/js/components-js/dean/deanNav.js
@@ -4,3 +4,20 @@ fetch('../components/dean-dashboard/dean-nav.html')
     document.getElementById('deanNav-placeholder').innerHTML = data;
     
   });
+
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.querySelector('.main-content');
+    const navbar = document.querySelector('.navbar');
+    
+    sidebar.classList.toggle('collapsed');
+    mainContent.classList.toggle('expanded');
+    
+    // Control navbar shift
+    if (sidebar.classList.contains('collapsed')) {
+        navbar.style.left = '80px';
+    } else {
+        navbar.style.left = '240px';
+    }
+}
