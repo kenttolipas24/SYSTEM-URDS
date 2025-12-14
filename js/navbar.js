370b857347b9b660d94b996a0fb54fdcf6602325
Xchnell380 <167052368+Xchnell380@users.noreply.github.com>
2025-12-14 15:33:52 +0800

    yadi daww kent

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,4 @@
-fetch('../components/navbar.html')
+fetch('../SYSTEM-URDS/components/navbar.html')
   .then(res => res.text())
   .then(data => {
     document.getElementById('navbar-placeholder').innerHTML = data;
